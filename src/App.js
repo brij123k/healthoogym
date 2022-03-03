f25d75a845414170ece0fdaf41b13b29bcdfe699
brij123k <brijk247@gmail.com>
2022-03-03 22:55:02 +0530

    My project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+
+const App = () => {
+  return (
+    <>
+    <Home/>
+    <Product/>
+    <ProductList/>
+    <Login/>
+    <Register/>
+    <Cart/>
+    </>
+  );
+};
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,77 @@
+// import first from "../imges/vegitable.jpg"
+export const sliderItems = [
+    {
+      id: 1,
+      img: '',
+      title: "TODAY SALE",
+      desc: "DON'T COMPROMISE ON HEALTH! GET FLAT 30% OFF FOR NEW ITEMS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "",
+      title: "BEST SALE",
+      desc: "DON'T COMPROMISE ON HEALTH! GET FLAT 30% OFF FOR NEW ITEMS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "",
+      title: "50% LESS EVERY PRODUCT",
+      desc: "DON'T COMPROMISE ON HEALTH! GET FLAT 30% OFF FOR NEW ITEMS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: " alt=img insert" ,
+      title: "meals",
+    },
+    {
+      id: 2,
+      img: "",
+      title: "Chicken",
+    },
+    {
+      id: 3,
+      img: "",
+      title: "Mutton",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"",
+    },
+    {
+      id:2,
+      img:"",
+    },
+    {
+      id:3,
+      img:"",
+    },
+    {
+      id:4,
+      img:"",
+    },
+    {
+      id:5,
+      img:"",
+    },
+    {
+      id:6,
+      img:"",
+    },
+    {
+      id:7,
+      img:"",
+    },
+    {
+      id:8,
+      img:"",
+    },
+  ]
